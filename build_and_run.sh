a2b2864430c8ef197a88be28cd99124ca2981b96
John Abanes <john.abanes@nyu.edu>
2026-04-26 16:59:36 -0400

    prepare for hardware deployment

--- build_and_run.sh
+++ build_and_run.sh
@@ -7,10 +7,14 @@ docker build -t "$IMAGE_NAME" "$SCRIPT_DIR"
 xhost +local:docker
 
 docker run -it --rm --name "$IMAGE_NAME" --network host \
+  --privileged \
   --gpus all \
   --runtime=nvidia \
   -e DISPLAY=$DISPLAY \
   -e NVIDIA_DRIVER_CAPABILITIES=all \
+  -v /dev:/dev \
+  -v /run/dbus:/run/dbus \
+  -v /var/run/dbus:/var/run/dbus \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v "$SCRIPT_DIR/src:/workspace/ros2_ws/src" \
   "$IMAGE_NAME"
